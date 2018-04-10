b055ea163418e31b6addb17aaea16a5dbd5391ce
SunMing <1229536801@qq.com>
2018-04-10 10:29:08 +0800

    完成Reduce类中的getFirstEven方法，实现功能：获取数组中第一个偶数

--- src/main/java/com/thoughtworks/collection/Reduce.java
+++ src/main/java/com/thoughtworks/collection/Reduce.java
@@ -7,60 +7,66 @@ import java.util.List;
 
 public class Reduce {
 
-    List<Integer> arrayList;
+  List<Integer> arrayList;
 
-    public Reduce(List<Integer> arrayList) {
-        this.arrayList = arrayList;
-    }
+  public Reduce(List<Integer> arrayList) {
+    this.arrayList = arrayList;
+  }
 
-    public int getMaximum() {
-        return Collections.max(arrayList);
-    }
+  public int getMaximum() {
+    return Collections.max(arrayList);
+  }
 
-    public double getMinimum() {
-        return Collections.min(arrayList);
-    }
+  public double getMinimum() {
+    return Collections.min(arrayList);
+  }
 
-    public double getAverage() {
-        double average=0.0;
-        for(int i:arrayList)
-            average+=i;
-        return average/arrayList.size();
+  public double getAverage() {
+    double average = 0.0;
+    for (int i : arrayList) {
+      average += i;
     }
+    return average / arrayList.size();
+  }
 
-    public double getOrderedMedian() {
-        if (arrayList.size() % 2 == 1) {
-            return arrayList.get(arrayList.size() / 2);
-        } else {
-            return
-                (double) (arrayList.get(arrayList.size() / 2) + arrayList
-                    .get(arrayList.size() / 2 - 1))
-                    / 2;
-        }
+  public double getOrderedMedian() {
+    if (arrayList.size() % 2 == 1) {
+      return arrayList.get(arrayList.size() / 2);
+    } else {
+      return
+          (double) (arrayList.get(arrayList.size() / 2) + arrayList
+              .get(arrayList.size() / 2 - 1))
+              / 2;
     }
+  }
 
-    public int getFirstEven() {
-        throw new NotImplementedException();
+  public int getFirstEven() {
+    for (int i : arrayList) {
+      if (i % 2 == 0) {
+        return i;
+      }
     }
+    return 0;
+  }
 
-    public int getIndexOfFirstEven() {
-        throw new NotImplementedException();
-    }
+  public int getIndexOfFirstEven() {
+    throw new NotImplementedException();
+  }
 
-    public boolean isEqual(List<Integer> arrayList) {
-        throw new NotImplementedException();
-    }
+  public boolean isEqual(List<Integer> arrayList) {
+    throw new NotImplementedException();
+  }
 
-    //实现接口SingleLink，然后再此函数内使用
-    public Double getMedianInLinkList(SingleLink singleLink) {
-        throw new NotImplementedException();
-    }
+  //实现接口SingleLink，然后再此函数内使用
+  public Double getMedianInLinkList(SingleLink singleLink) {
+    throw new NotImplementedException();
+  }
 
-    public int getLastOdd() {
-        throw new NotImplementedException();
-    }
+  public int getLastOdd() {
+    throw new NotImplementedException();
+  }
 
-    public int getIndexOfLastOdd() {
-        throw new NotImplementedException();
-    }
+  public int getIndexOfLastOdd() {
+    throw new NotImplementedException();
+  }
 }

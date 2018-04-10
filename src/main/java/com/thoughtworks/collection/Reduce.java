d5c9c670dc902330b435ede48d395ec5964f5153
SunMing <1229536801@qq.com>
2018-04-10 10:40:10 +0800

    完成Reduce类中的getLastOdd方法，实现功能：获取数组中最后一个奇数

--- src/main/java/com/thoughtworks/collection/Reduce.java
+++ src/main/java/com/thoughtworks/collection/Reduce.java
@@ -68,7 +68,12 @@ public class Reduce {
   }
 
   public int getLastOdd() {
-    throw new NotImplementedException();
+    for (int i = arrayList.size()-1; i >=0 ; i--) {
+      if (arrayList.get(i) % 2 == 1) {
+        return arrayList.get(i);
+      }
+    }
+    return -1;
   }
 
   public int getIndexOfLastOdd() {
